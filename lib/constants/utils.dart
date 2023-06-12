3842c0c7e308926f888ca106c01b0995b3c8dcb0
Muhamad Syamim Irfan Bin Ahmad Shokkri <syamimirfan59@gmail.com>
2023-06-12 23:12:32 +0800

    Backend development

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,6 +1,7 @@
 class Utils {
   //port from backend (node.js)
   //deployed from render
+  //static String baseURL = "http://localhost:5000";
   static String baseURL = "https://wemeet-dapps-backend.onrender.com";
 }
 
